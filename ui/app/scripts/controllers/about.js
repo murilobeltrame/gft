b072aa0cf92d2811be62287adeb0e384a3d54794
Murilo Beltrame <murilobeltrame@gmail.com>
2016-11-25 13:37:28 -0200

    First Commit

---
+++ ui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gftApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gftApp
+ */
+angular.module('gftApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
