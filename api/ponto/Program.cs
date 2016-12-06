702f19ecc177885bb041a75ff00464cb1f27dc85
Murilo Beltrame <murilobeltrame@gmail.com>
2016-12-06 16:31:48 -0200

    Add ponto API

---
+++ api/ponto/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace ponto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel(options => {
+                    options.NoDelay = true;
+                    options.UseConnectionLogging();
+                })
+                .UseUrls("http://0.0.0.0:8080", "https://0.0.0.0:8081")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
